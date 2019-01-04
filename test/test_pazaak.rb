3e702a968ba832cf0ecfa965ffb4238aeeb17e69
Dmitrii Sosedov <d.sosedov@gmail.com>
2019-01-03 21:09:26 -0500

    Fix module level tests

--- test/test_pazaak.rb
+++ test/test_pazaak.rb
@@ -10,7 +10,27 @@ class PazaakTest < Minitest::Test
     assert_equal([:VERSION], Pazaak.constants)
   end
 
-  #def test_all_constants
-  #  assert_equal([:MAX, :MIN, :REV, :VERSION], Pazaak.constants(false))
-  #end
+  def test_private_constants_max
+    ex = assert_raises NameError do
+      Pazaak::MAX
+    end
+
+    assert_equal 'private constant Pazaak::MAX referenced', ex.message
+  end
+
+  def test_private_constants_min
+    ex = assert_raises NameError do
+      Pazaak::MIN
+    end
+
+    assert_equal 'private constant Pazaak::MIN referenced', ex.message
+  end
+
+  def test_private_constants_rev
+    ex = assert_raises NameError do
+      Pazaak::REV
+    end
+
+    assert_equal 'private constant Pazaak::REV referenced', ex.message
+  end
 end
