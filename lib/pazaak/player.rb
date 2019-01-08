57bb301170ff8e62c03d4aa13e4e3437c693a49e
Dmitrii Sosedov <d.sosedov@gmail.com>
2019-01-07 21:38:16 -0500

    Add hit and total for player

--- lib/pazaak/player.rb
+++ lib/pazaak/player.rb
@@ -1,9 +1,18 @@
 module Pazaak
   class Player
-    attr_reader :name
+    attr_reader :name, :cards
 
     def initialize(name)
       @name = name
+      @cards = []
+    end
+
+    def hit(card)
+      @cards.push(card)
+    end
+
+    def total
+      @cards.inject(0){|sum, c| sum + c.value }
     end
   end
 end
--- test/pazaak/test_player.rb
+++ test/pazaak/test_player.rb
@@ -6,4 +6,13 @@ class GameTest < Minitest::Test
     player = Pazaak::Player.new('Carth')
     assert_equal('Carth', player.name)
   end
+
+  def test_hit_and_total
+    player = Pazaak::Player.new('Calo')
+    assert_equal(0, player.total)
+    player.hit(Pazaak::Card.new(10))
+    assert_equal(10, player.total)
+    player.hit(Pazaak::Card.new(5))
+    assert_equal(15, player.total)
+  end
 end
